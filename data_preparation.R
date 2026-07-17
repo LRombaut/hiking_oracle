d9ba5fbd42906396e20b31485ff6e5f3e840fba5
LRombaut <125313530+LRombaut@users.noreply.github.com>
2026-07-17 22:14:25 +0200

    Update data_preparation.R
    add day of week data for Sunday effect

--- data_preparation.R
+++ data_preparation.R
@@ -7,19 +7,36 @@ datetime <- paste(hikes$date, hikes$start_time, sep=" ")
 hikes$datetime <- as_datetime(datetime, format="%d/%m/%Y %H:%M")
 hikes$start_time <- hour(hikes$datetime)*60 + minute(hikes$datetime)
 hikes$date <- as.Date(hikes$datetime)
-hikes <- hikes[order(hikes$date),]
+hikes <- hikes[order(hikes$date),] # put hikes in order of date
 hikes$date_last_hike <- numeric(155)
 for(i in 1:length(hikes$date)){
   t <- hikes$date[i] - hikes$date[i-1]
   hikes$date_last_hike[i] <- as.numeric(t[1])
 }  
 hikes$date_last_hike[1] <- 0
+hikes$date_last_hike <- hikes$date_last_hike/7 - 1 #units of weeks since last hike iso days centred on 1 week
+hikes$month <- month(hikes$date)
 
 #remove hikes that have a signup cap
 sum(!is.na(hikes$cap))
 hikes <- hikes[which(is.na(hikes$cap)),]
 
+#add rainfall data
+rain_level <- numeric(124L)
+for(i in 1:length(rain_level)) rain_level[i] <- ifelse(hikes$hourly_precipitation_peak_mm[i] > 0, 1, 0) #binary indicator var for rain
+hikes$rain_level <- rain_level
 
+#Sunday effect
+dow <- weekdays(hikes$date)
+table(dow)
+
+for(i in 1:length(dow)){
+  dow[i] <- ifelse(dow[i] == "Sunday", 1, 0)
+}
+table(dow)
+hikes$dow <- as.integer(dow)
+
+#examine data distributions and summary statistics
 par(mfrow=c(1,5))
 hist(hikes$signups[hikes$special_category == "regular"])
 hist(hikes$signups[hikes$special_category == "heavy_hike"])
@@ -43,3 +60,7 @@ hist(hikes$Min.temp)
 hist(hikes$Max.temp)
 
 saveRDS(hikes, file="hikes.RDS")
+
+
+
+
